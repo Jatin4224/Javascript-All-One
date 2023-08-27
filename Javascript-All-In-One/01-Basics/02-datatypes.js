b85ff6dfea19fde05436a61ecf963ff200634467
jatin sharma <93386551+Jatin4224@users.noreply.github.com>
2023-08-27 13:35:53 +0530

    Add files via upload

---
+++ Javascript-All-In-One/01-Basics/02-datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)//0p-error because  we are using nodejs, not browser
+
+console.log(3 + 3); //code readability should be high
+
+//data types-
+//number -> 2 to power 53
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined ->
+//symbol
+
+//typeof-
+// used to determine the data type of a value. It returns a string representing the type of the operand
+
+console.log(typeof undefined); //op-undefined (js bug)
+console.log(typeof null); //op-object (js bug)
+console.log(typeof String);
