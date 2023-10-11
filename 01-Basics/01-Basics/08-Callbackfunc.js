c8c4c319f06ab3ea750ae89db9c3185c5ec4836d
jatin sharma <93386551+Jatin4224@users.noreply.github.com>
2023-10-12 02:06:50 +0530

    Update 08-Callbackfunc.js

--- 01-Basics/01-Basics/08-Callbackfunc.js
+++ 01-Basics/01-Basics/08-Callbackfunc.js
@@ -9,3 +9,23 @@ x(function y (){
 //javascript is a synchronous and single-threaded language but due to callbacks we can do async things inside javascript.
 
  
+const sumOrProduct = (
+  firstNumber,
+  secondNumber,
+  string,
+  callback
+) => {
+  let result = 0;
+  if (string === "sum") {
+    result = firstNumber + secondNumber;
+  } else {
+    result = firstNumber * secondNumber;
+  };
+
+  callback(string, result);
+};
+
+const printResult = (string, result) => console.log(`The solution to the ${string} is ${result}.`);
+
+sumOrProduct(3, 4, "sum", printResult);
+sumOrProduct(6, 8, "product", printResult);
