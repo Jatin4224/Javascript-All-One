dc0d45d96d4763609fa19a88704a7242725440a3
jatin sharma <93386551+Jatin4224@users.noreply.github.com>
2023-10-19 15:59:26 +0530

    Update 11-Array.js

--- 01-Basics/01-Basics/11-Array.js
+++ 01-Basics/01-Basics/11-Array.js
@@ -19,3 +19,10 @@ const evenNumbers = [2, 4, 6];
 const oddNumbers = [1, 3, 5];
 const numbers = evenNumbers.concat(oddNumbers);
 console.log(numbers);
+Filter elements based on given condition
+The filter() method creates a new array containing only certain elements from the array it is invoked on.
+
+filter() accepts a function as an argument. It invokes that function on each element of an array.
+const numbers = [1, 2, 3, 4, 5, 6];
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+console.log(evenNumbers);
