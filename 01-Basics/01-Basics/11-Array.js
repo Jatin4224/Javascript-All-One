6d8032fdc508abe815abafc7a153d3f82dd6fcb7
jatin sharma <93386551+Jatin4224@users.noreply.github.com>
2023-10-19 14:34:52 +0530

    Create 11-Array.js

---
+++ 01-Basics/01-Basics/11-Array.js
@@ -0,0 +1,8 @@
+Concatenate Elements Into New Array
+Concatenate means to join two things in succession.
+
+The concat() method creates a new array that contains elements from the array on which concat is invoked followed by the arguments passed. If the argument is an array, the elements of the array will be inserted into the new array.
+  const evenNumbers = [2, 4, 6];
+const oddNumbers = [1, 3, 5];
+const numbers = evenNumbers.concat(oddNumbers);
+console.log(numbers);
